b924d8b30d3d50cfd5cea9645dcfb99900cb1f46
Binkowski-D <binkowski.dam@gmail.com>
2024-09-29 13:51:09 +0200

    Added rounding to 2 decimal places using RoundingMode.HALF_UP.

--- src/calculator_strategy/DivideOperation.java
+++ src/calculator_strategy/DivideOperation.java
@@ -1,12 +1,13 @@
 package calculator_strategy;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class DivideOperation implements Operation{
     @Override
     public BigDecimal execute(BigDecimal currentValue, BigDecimal operand) {
         if(operand.compareTo(BigDecimal.ZERO) != 0){
-            return currentValue.divide(operand);
+            return currentValue.divide(operand, 2, RoundingMode.HALF_UP);
         }else{
             throw new ArithmeticException("Do not divide by 0");
         }
